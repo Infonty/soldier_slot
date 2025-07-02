b48515b108fbfc42993af37de2a6539457ea626d
infonty <infonty919344@gmail.com>
2025-07-02 21:55:20 +0530

    code move

--- resources/views/layouts/app/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<section id="footer" class="bg-dark text-light p-5" style="border-top:1px solid #df9333;">
-    <p class="text-center">PickTime&#169;2023</p>
-</section>
--- resources/views/user/confirm.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('main')
-<section id="success" class="bg-dark" style="min-height:600px;">
-    <div class="container text-center">
-        <img src="assets/images/verified.gif" class="img-fluid" alt="">
-    </div>
-</section>
-@endsection
